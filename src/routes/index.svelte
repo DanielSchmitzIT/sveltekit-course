7d0d110923d12a67be22892c9029ab51a89765a0
Daniel Schmitz <daniel.schmitz@schmitz-dynamics.ch>
2021-10-04 20:49:55 +0200

    Adding Google Font

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,4 @@
 <script>
-	import Counter from '../UI/Counter.svelte';
 
 	let startCount = 5;
 
@@ -12,8 +11,11 @@
 	}
 </script>
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href='https://kit.svelte.dev'>kit.svelte.dev</a> to read the documentation</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, quam!</p>
+
+<!--
 <button>Test</button>
 <Counter on:maxReached={(evt) => console.log(evt.detail)} count={5} min={-5} max={10} />
 <Counter on:minReached={handleMinReached} on:maxReached={handleMaxReached} count={2} min={-5} max={15} />
 {startCount}
+-->
