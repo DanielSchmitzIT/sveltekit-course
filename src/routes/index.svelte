90fa99b420922a352edbc3676385b419c4d5d0da
Daniel Schmitz <daniel.schmitz@schmitz-dynamics.ch>
2021-10-04 20:01:21 +0200

    Initial commit - scoping passing property

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,10 @@
 <script>
 	import Counter from '../UI/Counter.svelte';
+	let startCount = 5;
 </script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href='https://kit.svelte.dev'>kit.svelte.dev</a> to read the documentation</p>
 <button>Test</button>
-<Counter></Counter>
+<Counter bind:count={startCount} />
+<Counter bind:count={startCount} />
+{startCount}
