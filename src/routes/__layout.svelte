8cf05ec2294c27e35198f47af8fd2ae809467712
Daniel Schmitz <daniel.schmitz@schmitz-dynamics.ch>
2021-10-05 20:05:23 +0200

    style updates

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -32,7 +32,9 @@
         width: 1280px;
         padding: 30px;
         box-shadow: 0 3px 1px -2px rgb(0 0 0 / 20%), 0 2px 2px 0 rgb(0 0 0 / 14%), 0 1px 5px 0 rgb(0 0 0 / 12%);
-        margin: 20px;
+        margin: 40px;
+        background: white;
+        border-radius: 10px;
     }
 </style>
 
