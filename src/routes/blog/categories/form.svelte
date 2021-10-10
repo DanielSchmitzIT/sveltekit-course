bcc245de3e151c920ff98c2035ff5ecb8f64158d
Daniel Schmitz <daniel.schmitz@schmitz-dynamics.ch>
2021-10-10 23:15:25 +0200

    Submit category form

--- src/routes/blog/categories/form.svelte
+++ src/routes/blog/categories/form.svelte
@@ -1,23 +1,45 @@
 <script lang='ts'>
 	import { Category } from '$lib/category';
+	import { browser } from '$app/env';
+	import { goto } from '$app/navigation';
 
 	const category: Category = {
 		title: null,
 		img: null
 	};
+
+	let files: FileList | [] = [];
+
+	let submit = async ($event) => {
+		const response = await fetch('/api/blog/categories', {
+			method: 'POST',
+			body: JSON.stringify(category)
+		});
+		const addedCategory = await response.json();
+		await goto('/blog/categories');
+	};
+
+	$: {
+		if (browser && files instanceof FileList) {
+			let reader = new FileReader();
+			reader.readAsDataURL(files.item(0));
+			reader.onload = () => category.img = reader.result.toString();
+			reader.onerror = () => console.log('Error reading file');
+		}
+	}
 </script>
 
 <h2 class='text-center'>
 	Add a new category
 </h2>
-<form action=''>
+<form on:submit|preventDefault={submit}>
 	<div class='form-group'>
 		<label for='title'>Title</label>
-		<input type='text' placeholder='Title of category (e.g. Hardware)' id='title'>
+		<input bind:value='{category.title}' type='text' placeholder='Title of category (e.g. Hardware)' id='title'>
 	</div>
 	<div class='form-group'>
 		<label for='img'>File</label>
-		<input type='file' id='img' accept='image/*'>
+		<input bind:files type='file' id='img' accept='image/*'>
 	</div>
 	<div class='actions'>
 		<div>
@@ -31,14 +53,10 @@
 </form>
 
 <style lang='scss'>
-  @import "static/scss/variables";
-
 
   .actions {
     display: flex;
     justify-content: space-between;
   }
-
-
 
 </style>
