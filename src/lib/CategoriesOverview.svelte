29905b2122949c9aa637820e467610895a3964fe
Daniel Schmitz <daniel.schmitz@schmitz-dynamics.ch>
2021-10-10 16:09:07 +0200

    Categories - Wrapping up

---
+++ src/lib/CategoriesOverview.svelte
@@ -0,0 +1,51 @@
+<script lang='ts'>
+	export let categories = [];
+</script>
+
+
+<div class='categories-wrapper'>
+	{#each categories as category}
+		<a sveltekit:prefetch class='category' href='categories/{category.title}'>
+			<img src='{category.img}' alt='Information Technology'>
+			<span class='title'>
+				{category.title}
+			</span>
+		</a>
+	{/each}
+</div>
+
+
+<style>
+    a.category {
+        display: block;
+        flex: 1;
+        position: relative;
+        max-width: 400px;
+        margin-right: 20px;
+        min-width: 400px;
+    }
+
+    .categories-wrapper {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+
+    img {
+        width: 100%;
+    }
+
+    span.title {
+        position: absolute;
+        bottom: 20px;
+        left: 20px;
+        font-size: 22px;
+        color: white;
+        background: rgba(0, 0, 0, 0.5);
+        padding: 10px;
+        border-radius: 10px;
+        box-shadow: 2px 2px 2px black;
+    }
+</style>
+
--- src/routes/blog/categories/index.svelte
+++ src/routes/blog/categories/index.svelte
@@ -1,62 +1,8 @@
+<script lang='ts'>
+	import { categories } from '$lib/database';
+	import CategoriesOverview from '$lib/CategoriesOverview.svelte';
+</script>
+
 <h1>Categories Index</h1>
 
-<div class='categories-wrapper'>
-	<a sveltekit:prefetch class='category' href='categories/information-technology'>
-		<img src='/categories/information-technology.jpg' alt='Information Technology'>
-		<span class='title'>
-			Information - Technology
-		</span>
-	</a>
-	<a sveltekit:prefetch class='category' href='categories/business'>
-		<img src='/categories/business.jpg' alt='Business'>
-		<span class='title'>
-			Business
-		</span>
-	</a>
-	<a sveltekit:prefetch class='category' href='categories/marketing'>
-		<img src='/categories/marketing.jpg' alt='Marketing'>
-		<span class='title'>
-			Marketing
-		</span>
-	</a>
-	<a sveltekit:prefetch class='category' href='categories/work-life-balance'>
-		<img src='/categories/work-life-balance.jpg' alt='Relax'>
-		<span class='title'>
-			Work life balance
-		</span>
-	</a>
-</div>
-
-<style>
-    a.category {
-        display: block;
-        flex: 1;
-        position: relative;
-        max-width: 400px;
-        margin-right: 20px;
-        min-width: 400px;
-    }
-
-    .categories-wrapper {
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-        justify-content: center;
-    }
-
-    img {
-        width: 100%;
-    }
-
-    span.title {
-        position: absolute;
-        bottom: 20px;
-        left: 20px;
-        font-size: 22px;
-        color: white;
-        background: rgba(0, 0, 0, 0.5);
-        padding: 10px;
-        border-radius: 10px;
-        box-shadow: 2px 2px 2px black;
-    }
-</style>
+<CategoriesOverview categories='{categories}' />
