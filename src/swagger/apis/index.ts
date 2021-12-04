3ad041ae0de7a1a694e83169346b0797045016c1
Daniel Schmitz <daniel.schmitz@schmitz-dynamics.ch>
2021-12-04 23:07:49 +0100

    Remove swagger

--- src/swagger/apis/index.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './AuthApi';
-export * from './CategoryApi';
-export * from './ImageApi';
-export * from './PostApi';
-export * from './UserApi';
-export * from './ViewApi';
